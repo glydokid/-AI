a34fc56ef8acb836e5d0118ed99fb04c15c93d6a
glydokid <glydokidd@naver.com>
2022-01-12 16:38:52 +0900

    데이터 확인

---
+++ CycleGan/데이터 확인.py
@@ -0,0 +1,21 @@
+'''
+데이터 셋트의 이미지를 3x3으로 보여주는 플롯 생성
+'''
+from numpy import load
+from matplotlib import pyplot
+# load the dataset
+data = load('data_set_256.npz')
+dataA, dataB = data['arr_0'], data['arr_1']
+print('Loaded: ', dataA.shape, dataB.shape)
+# plot source images
+n_samples = 3
+for i in range(n_samples):
+	pyplot.subplot(2, n_samples, 1 + i)
+	pyplot.axis('off')
+	pyplot.imshow(dataA[i].astype('uint8'))
+# plot target image
+for i in range(n_samples):
+	pyplot.subplot(2, n_samples, 1 + n_samples + i)
+	pyplot.axis('off')
+	pyplot.imshow(dataB[i].astype('uint8'))
+pyplot.show()
